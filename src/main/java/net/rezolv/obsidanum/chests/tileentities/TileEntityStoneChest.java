b7c559f595ae691a9832e25ee982e63c9f837bcd
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2024-06-09 01:41:23 +0300

    Rename chest gui

--- src/main/java/net/rezolv/obsidanum/chests/tileentities/TileEntityStoneChest.java
+++ src/main/java/net/rezolv/obsidanum/chests/tileentities/TileEntityStoneChest.java
@@ -2,13 +2,16 @@ package net.rezolv.obsidanum.chests.tileentities;
 
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.rezolv.obsidanum.chests.SCRegistry;
 import net.rezolv.obsidanum.chests.blocks.EnumStoneChest;
 
 public class TileEntityStoneChest extends ChestBlockEntity {
-
+    protected Component getDefaultName() {
+        return Component.translatable("container.chest_obsidian");
+    }
     private EnumStoneChest chestType;
 
     public TileEntityStoneChest(BlockPos pos, BlockState state) {
