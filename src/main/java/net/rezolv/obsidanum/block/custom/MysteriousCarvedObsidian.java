c8b04ada2ddbdf998ba24ae40cbd6bbecee5e8f7
Aleks <Aleksander.zhuckov2011@gmail.com>
2024-08-15 17:41:02 +0300

    Fix structure

--- src/main/java/net/rezolv/obsidanum/block/custom/MysteriousCarvedObsidian.java
+++ src/main/java/net/rezolv/obsidanum/block/custom/MysteriousCarvedObsidian.java
@@ -3,17 +3,15 @@ package net.rezolv.obsidanum.block.custom;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraftforge.event.entity.EntityEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class MysteriousCarvedObsidian extends Block {
     public static final BooleanProperty MYSTERIOUS = BooleanProperty.create("mysterious");
+    private static final int DETECTION_RADIUS_SQ = 15; // 15 blocks squared
 
     public MysteriousCarvedObsidian(Properties pProperties) {
         super(pProperties);
@@ -22,7 +20,8 @@ public class MysteriousCarvedObsidian extends Block {
     @Override
     public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
         updateMysteriousState(level, pos);
-        level.scheduleTick(pos, this, 20); // Reschedule tick
+        // Запланировать следующий тик через 20 тиков (1 секунда)
+        level.scheduleTick(pos, this, 8);
     }
 
     @Override
@@ -30,14 +29,16 @@ public class MysteriousCarvedObsidian extends Block {
         builder.add(MYSTERIOUS);
     }
 
-
-
-
     private void updateMysteriousState(Level level, BlockPos pos) {
         boolean hasNearbyPlayer = level.players().stream()
-                .anyMatch(player -> player.distanceToSqr(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5) <= 15); // Distance squared for 3 blocks
+                .anyMatch(player -> player.distanceToSqr(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5) <= DETECTION_RADIUS_SQ);
 
-        BlockState newState = this.defaultBlockState().setValue(MYSTERIOUS, hasNearbyPlayer);
-        level.setBlock(pos, newState, 3);
+        BlockState currentState = level.getBlockState(pos);
+        if (currentState.getValue(MYSTERIOUS) != hasNearbyPlayer) {
+            BlockState newState = currentState.setValue(MYSTERIOUS, hasNearbyPlayer);
+            level.setBlock(pos, newState, 3);
+            // Уведомить клиентов об обновлении блока
+            level.sendBlockUpdated(pos, currentState, newState, 3);
+        }
     }
 }
